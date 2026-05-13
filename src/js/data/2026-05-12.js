5643e3ffcaa8df208545fedbd12b08b9038f9395
etherealblast <alejandrocastroaraya@gmail.com>
2026-05-13 17:25:34 -0600

    Added Ys 1 and 2 characters

--- src/js/data/2026-05-12.js
+++ src/js/data/2026-05-12.js
@@ -11,10 +11,11 @@ dataSet[dataSetVersion].options = [
       { name: "Ys: Origin", key: "Origin" },
       { name: "Ys I: Ancient Ys Vanished", key: "I" },
       { name: "Ys II: Ancient Ys Vanished – The Final Chapter", key: "II" },
-      { name: "Ys III / Oath in Felghana", key: "III" },
-      { name: "Ys IV Mask of the Sun / Dawn of Ys / Memories of Celceta", key: "IV" },
+      { name: "Ys: The Oath in Felghana", key: "III" },
+      { name: "Ys: Memories of Celceta", key: "IV" },
       { name: "Ys V: Lost Kefin, Kingdom of Sand", key: "V" },
-      { name: "Ys VI: The Ark of Napishtim / Ys VI Online", key: "VI" },
+      { name: "Ys VI: The Ark of Napishtim", key: "VI" },
+      { name: "Ys VI Online", key: "VIOnline" },
       { name: "Ys Seven", key: "VII" },
       { name: "Ys VIII: Lacrimosa of Dana", key: "VIII" },
       { name: "Ys IX: Monstrum Nox", key: "IX" },
@@ -60,17 +61,10 @@ dataSet[dataSetVersion].characterData = [
     }
   },
   {
-    name: "Adol Christin",
-    img: "adol.jpg",
-    opts: {
-      game: ["Origin"]
-    }
-  },
-  {
     name: "Reah",
     img: "reah.jpg",
     opts: {
-      game: ["Origin"],
+      game: ["Origin", "I", "II"],
       NPC: true
     }
   },
@@ -78,7 +72,7 @@ dataSet[dataSetVersion].characterData = [
     name: "Feena",
     img: "feena.jpg",
     opts: {
-      game: ["Origin"],
+      game: ["Origin", "I", "II"],
       NPC: true
     }
   },
@@ -190,7 +184,7 @@ dataSet[dataSetVersion].characterData = [
     name: "Zava",
     img: "zava.jpg",
     opts: {
-      game: ["Origin"],
+      game: ["Origin", "II"],
       NPC: true
     }
   },
@@ -198,7 +192,7 @@ dataSet[dataSetVersion].characterData = [
     name: "Dalles",
     img: "dalles.jpg",
     opts: {
-      game: ["Origin"],
+      game: ["Origin", "II"],
       NPC: true
     }
   },
@@ -206,7 +200,7 @@ dataSet[dataSetVersion].characterData = [
     name: "Darm",
     img: "darm.jpg",
     opts: {
-      game: ["Origin"],
+      game: ["Origin", "II"],
       NPC: true
     }
   },
@@ -214,10 +208,244 @@ dataSet[dataSetVersion].characterData = [
     name: "Cain Fact",
     img: "cain.jpg",
     opts: {
-      game: ["Origin"],
+      game: ["Origin", "II"],
       NPC: true
     }
   },
   // Ys I
+  {
+    name: "Adol Christin",
+    img: "adol.jpg",
+    opts: {
+      game: ["I", "II", "III", "IV", "V", "VI", "VIOnline", "VII", "VIII", "IX", "X"]
+    }
+  },
+  {
+    name: "Dogi",
+    img: "dogi.jpg",
+    opts: {
+      game: ["I", "II", "III", "IV", "V", "VI", "VIOnline", "VII", "VIII", "IX", "X"],
+      NPC: true
+    }
+  },
+  {
+    name: "Sara Tovah",
+    img: "sara.jpg",
+    opts: {
+      game: ["I"],
+      NPC: true
+    }
+  },
+  {
+    name: "Jeba Tovah",
+    img: "jeba.jpg",
+    opts: {
+      game: ["I"],
+      NPC: true
+    }
+  },
+  {
+    name: "Goban Tovah",
+    img: "goban.jpg",
+    opts: {
+      game: ["I", "II"],
+      NPC: true
+    }
+  },
+  {
+    name: "Luta Gemma",
+    img: "luta.jpg",
+    opts: {
+      game: ["I", "II"],
+      NPC: true
+    }
+  },
+  {
+    name: "Raba Gemma",
+    img: "raba.jpg",
+    opts: {
+      game: ["I", "VI", "VIOnline"],
+      NPC: true
+    }
+  },
+  {
+    name: "Slaff",
+    img: "slaff.jpg",
+    opts: {
+      game: ["I"],
+      NPC: true
+    }
+  },
+  {
+    name: "Doctor Bludo",
+    img: "drbludo.jpg",
+    opts: {
+      game: ["I"],
+      NPC: true
+    }
+  },
+  {
+    name: "Nurse Ayla",
+    img: "ayla.jpg",
+    opts: {
+      game: ["I"],
+      NPC: true
+    }
+  },
+  {
+    name: "Rosetty",
+    img: "rosetty.jpg",
+    opts: {
+      game: ["I"],
+      NPC: true
+    }
+  },
+  {
+    name: "Dios",
+    img: "dios.jpg",
+    opts: {
+      game: ["I"],
+      NPC: true
+    }
+  },
+  {
+    name: "Pim",
+    img: "pim.jpg",
+    opts: {
+      game: ["I"],
+      NPC: true
+    }
+  },
+  {
+    name: "Nurse Lisa",
+    img: "lisa.jpg",
+    opts: {
+      game: ["I"],
+      NPC: true
+    }
+  },
+  {
+    name: "Mayor Robels",
+    img: "robels.jpg",
+    opts: {
+      game: ["I"],
+      NPC: true
+    }
+  },
+  {
+    name: "Dark Fact",
+    img: "darkfact.jpg",
+    opts: {
+      game: ["I"],
+      NPC: true
+    }
+  },
+  {
+    name: "Jeanne",
+    img: "jeanne.jpg",
+    opts: {
+      game: ["I", "II"],
+      NPC: true,
+      noncanon: true
+    }
+  },
+  {
+    name: "Misha",
+    img: "misha.jpg",
+    opts: {
+      game: ["I", "II"],
+      NPC: true,
+      noncanon: true
+    }
+  },
+  // Ys II
+  {
+    name: "Lilia",
+    img: "lilia.jpg",
+    opts: {
+      game: ["II"],
+      NPC: true
+    }
+  },
+  {
+    name: "Tarf Hadal",
+    img: "tarf.jpg",
+    opts: {
+      game: ["II"],
+      NPC: true
+    }
+  },
+  {
+    name: "Keith Fact",
+    img: "keith.jpg",
+    opts: {
+      game: ["II"],
+      NPC: true
+    }
+  },
+  {
+    name: "Maria Mesa",
+    img: "maria.jpg",
+    opts: {
+      game: ["II"],
+      NPC: true
+    }
+  },
+  {
+    name: "Gorto Dabbie",
+    img: "gorto.jpg",
+    opts: {
+      game: ["II"],
+      NPC: true
+    }
+  },
+  {
+    name: "Sada",
+    img: "sada.jpg",
+    opts: {
+      game: ["II"],
+      NPC: true
+    }
+  },
+  {
+    name: "Hadat",
+    img: "hadat.jpg",
+    opts: {
+      game: ["II"],
+      NPC: true
+    }
+  },
+  {
+    name: "Flair Rall",
+    img: "flair.jpg",
+    opts: {
+      game: ["II", "X"],
+      NPC: true
+    }
+  },
+  {
+    name: "Guido",
+    img: "flair.jpg",
+    opts: {
+      game: ["II"],
+      NPC: true
+    }
+  },
+  {
+    name: "Jade",
+    img: "jade.jpg",
+    opts: {
+      game: ["II"],
+      NPC: true
+    }
+  },
+  {
+    name: "Zalem",
+    img: "zalem.jpg",
+    opts: {
+      game: ["II"],
+      NPC: true
+    }
+  },
 ];
 	
